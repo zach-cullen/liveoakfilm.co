889d2b4c01d3b393748a1553fe8a2f8bb816b1b9
Zach Cullen <zacullen@gmail.com>
2020-07-31 17:32:49 -0500

    create theme module

---
+++ theme/theme.js
@@ -0,0 +1,27 @@
+export default {
+  colors: {
+    amber: {
+      hex: '#ae804c',
+      hsl: 'hsl(32, 39%, 49%)'
+    },
+    charcoal: {
+      hex: '#303030',
+      hsl: 'hsl(0, 0%, 19%)'
+    },
+    stone: {
+      hex: '#f8f5f1',
+      hsl: 'hsl(36, 34%, 96%)'
+    }
+  },
+  sizes: {
+    text: {
+      p: 20,
+      h6: 12,
+      h5: 16,
+      h4: 20,
+      h3: 24,
+      h2: 32,
+      h1: 48
+    }
+  }
+}
