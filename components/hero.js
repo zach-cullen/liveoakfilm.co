860c50ab3434c074d34336ae92f613f6bfa721ad
Zach Cullen <zacullen@gmail.com>
2020-07-31 18:07:37 -0500

    create basic hero component rendering styled sibiling

---
+++ components/hero.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  height: 400px;
+  width: 100%;
+  background-color: yellow;
+`
+
+const Hero = () => {
+  return (
+    <StyledDiv />
+  )
+}
+
+export default Hero
