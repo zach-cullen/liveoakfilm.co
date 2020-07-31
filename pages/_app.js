832485f0d2d7d82881b7c86bda1295eefd75ec13
Zach Cullen <zacullen@gmail.com>
2020-07-31 17:33:31 -0500

    wrap pages in theme provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import '../styles/reset.css'
+import { ThemeProvider } from 'styled-components'
+import theme from '../theme/theme'
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default App
