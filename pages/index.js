558d567a162a66320c57f1bc9cca5709cc06b61d
Zach Cullen <zacullen@gmail.com>
2020-08-02 09:17:13 -0500

    recompose page using pagesection wrappers

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Hero from '../components/hero'
 import Button from '../components/button'
+import PageSection from '../components/PageSection'
 
 export default function Home () {
   return (
@@ -11,13 +12,17 @@ export default function Home () {
       </Head>
 
       <main>
-        <Hero>
-          Hero
-        </Hero>
-        <h1>Main</h1>
-        <Button>
-          Hello!
-        </Button>
+        <Hero />
+        <PageSection>
+          <h1>Hello!</h1>
+          <Button>
+            Contact
+          </Button>
+        </PageSection>
+        <PageSection>
+          <h1>See you later.</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat est. Auctor eu augue ut lectus arcu. Pharetra massa massa ultricies mi quis. Pretium lectus quam id leo in vitae turpis massa.</p>
+        </PageSection>
       </main>
 
       <footer>
