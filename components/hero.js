6bcf3c31227d6f6eff002b2e32ef6b69c2b2d0a7
Zach Cullen <zacullen@gmail.com>
2020-08-02 09:15:52 -0500

    create hero as composition of styled divs with responsive video background

--- components/hero.js
+++ components/hero.js
@@ -1,15 +1,43 @@
 import styled from 'styled-components'
 
-const StyledDiv = styled.div`
-  height: 400px;
+const HeroDiv = styled.div`
+  position: relative;
   width: 100%;
-  background-color: yellow;
+  height: 60vh;
+  background-color: hsla(32, 29%, 35%, 40%);
 `
 
-const Hero = () => {
-  return (
-    <StyledDiv />
-  )
-}
+const BackgroundVideoWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  pointer-events: none;
+  overflow: hidden;
+`
+
+const BackgroundVideoIframe = styled.iframe`
+  width: 100vw;
+  height: 56.25vw;
+  min-height: 70vh;
+  min-width: 125vh;
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, -10%);
+`
+
+const Hero = () => (
+  <HeroDiv>
+    <BackgroundVideoWrapper>
+      <BackgroundVideoIframe
+        src='https://player.vimeo.com/video/227477654?background=1&autoplay=1&loop=1&byline=0&title=0'
+        frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen
+      />
+    </BackgroundVideoWrapper>
+  </HeroDiv>
+)
 
 export default Hero
