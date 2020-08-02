fb91d4b987eac22548d30addb1dfb36b3fe4ba7d
Zach Cullen <zacullen@gmail.com>
2020-08-02 09:16:38 -0500

    create page section component to apply container styles

---
+++ components/PageSection.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const PageSection = styled.div`
+  width: 100%;
+  min-height: 400px;
+  background-color: ${props => props.theme.colors.stone.hex};
+`
+
+export default PageSection
