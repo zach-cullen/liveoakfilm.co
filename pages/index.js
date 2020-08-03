a9da8aeb81957c9ab6b2d81ef79bd626e4a9eda1
Zach Cullen <zacullen@gmail.com>
2020-08-03 14:07:14 -0500

    change h1s to h2

--- pages/index.js
+++ pages/index.js
@@ -17,13 +17,13 @@ export default function Home () {
           videoUrl='https://player.vimeo.com/video/227477654?background=1&autoplay=1&loop=1&byline=0&title=0'
         />
         <PageSection>
-          <h1>Hello!</h1>
+          <h2>Hello!</h2>
           <Button>
             Contact
           </Button>
         </PageSection>
         <PageSection>
-          <h1>See you later.</h1>
+          <h2>See you later.</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat est. Auctor eu augue ut lectus arcu. Pharetra massa massa ultricies mi quis. Pretium lectus quam id leo in vitae turpis massa.</p>
         </PageSection>
       </main>
