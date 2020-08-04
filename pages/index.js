a601e46a66fe15a042a6b43a2a330f7f0a49897a
Zach Cullen <zacullen@gmail.com>
2020-08-04 12:12:53 -0500

    use container and text components to style first page section

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Hero from '../components/Hero'
 import Button from '../components/Button'
 import PageSection from '../components/PageSection'
+import SectionContainer from '../components/SectionContainer'
+import CustomContainer from '../components/CustomContainer'
+import Text from '../components/Text'
 
 export default function Home () {
   return (
@@ -17,10 +20,20 @@ export default function Home () {
           videoUrl='https://player.vimeo.com/video/227477654?background=1&autoplay=1&loop=1&byline=0&title=0'
         />
         <PageSection>
-          <h1>Love should be<br />remembered.</h1>
-          <Button>
-            Contact
-          </Button>
+          <SectionContainer textAlign='center'>
+            <CustomContainer maxW='540px'>
+              <h1>
+                Love should be<br />
+                remembered.
+              </h1>
+              <Text pY='30px'>
+                These are the memories that you want to last forever. This is the feeling that you don't want to forget. We are here to help you capture the best moments of your life because a love that is remembered is a <strong>love that leaves a legacy.</strong>
+              </Text>
+            </CustomContainer>
+            <Button>
+              Contact
+            </Button>
+          </SectionContainer>
         </PageSection>
         <PageSection>
           <h2>See you later.</h2>
