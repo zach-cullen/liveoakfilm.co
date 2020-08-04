aeadd6b3dc81aa4885d954be7c2e39939bedca38
Zach Cullen <zacullen@gmail.com>
2020-08-04 13:56:54 -0500

    update color prop to reflect theme reconfiguration

--- components/PageSection.js
+++ components/PageSection.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const PageSection = styled.div`
   width: 100%;
   padding: 60px 0;
-  background-color: ${props => props.theme.colors.stone.hex};
+  background-color: ${props => props.theme.colors.stone};
 `
 
 export default PageSection
