480db21938996ba26482cbf6998b31c666a9e030
Zach Cullen <zacullen@gmail.com>
2020-08-04 13:55:14 -0500

    restyle button and add hover transition

--- components/Button.js
+++ components/Button.js
@@ -1,14 +1,32 @@
 import styled from 'styled-components'
 
 const Button = styled.button`
-  border: none;
   cursor: pointer;
-  width: 100px;
-  height: 50px;
-  border-radius: 2px;
-  background-color: ${props => props.theme.colors.amber.hex};
-  color: white;a
+  border-radius: 5px;
+  padding: 20px 25px;
+  margin: 10px 0;
+  box-sizing: border-box;
+  border: solid 2px ${props => props.theme.colors.amber};
+  background-color: transparent;
+  color: ${props => props.theme.colors.amber};
+  font-family: Montserrat, sans-serif;
+  font-size: 11px;
+  text-transform: uppercase;
+  letter-spacing: 0.25em;
   text-align: center;
+  transition: background-color 0.2s, color 0.2s, box-shadow 0.2s;
+
+  &:hover {
+    color: #fff;
+    background-color: ${props => props.theme.colors.amber};
+  }
+
+  &:focus {
+    outline: none;
+    border: solid 2px ${props => props.theme.colors.amber};
+  }
+
+
 `
 
 export default Button
