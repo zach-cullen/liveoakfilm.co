f32099436cde4356a07b21356b9bac23b7ae016e
Zach Cullen <zacullen@gmail.com>
2020-08-04 14:20:26 -0500

    create styled footer component

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Footer = styled.footer`
+  width: 100%;
+  box-sizing: border-box;
+  padding: 50px 0;
+  background-color: ${props => props.theme.colors.charcoal};
+  color: white;
+`
+
+export default Footer
