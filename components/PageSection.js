6b717cc9493703a13ac36a4bef566ccb69cc02e9
Zach Cullen <zacullen@gmail.com>
2020-08-04 14:00:49 -0500

    use bgColor prop to set background color instead of theme

--- components/PageSection.js
+++ components/PageSection.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const PageSection = styled.div`
   width: 100%;
   padding: 60px 0;
-  background-color: ${props => props.theme.colors.stone};
+  background-color: ${props => props.bgColor};
 `
 
 export default PageSection
