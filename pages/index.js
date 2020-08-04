29c0ad41f8af4b8cbe831d7dab73a397a6693125
Zach Cullen <zacullen@gmail.com>
2020-08-04 10:35:13 -0500

    add section 1 headline

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home () {
           videoUrl='https://player.vimeo.com/video/227477654?background=1&autoplay=1&loop=1&byline=0&title=0'
         />
         <PageSection>
-          <h2>Hello!</h2>
+          <h1>Love should be<br />remembered.</h1>
           <Button>
             Contact
           </Button>
