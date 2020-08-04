d739e2390d90ff9792f6d6a9c3c0487b99b6feca
Zach Cullen <zacullen@gmail.com>
2020-08-04 12:10:30 -0500

    create section container styled component for outermost width boundaries

---
+++ components/SectionContainer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const SectionContainer = styled.div`
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 0 20px;
+  text-align: ${props => props.textAlign};
+`
+
+export default SectionContainer
