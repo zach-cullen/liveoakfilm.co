f8bfde7c4a36efbff90c6129cab64190647b8d5f
Zach Cullen <zacullen@gmail.com>
2020-08-05 14:57:26 -0500

    change video gallery from grid of 4 to single featured video with text

--- components/VideoGallery.js
+++ components/VideoGallery.js
@@ -1,24 +1,34 @@
 import styled from 'styled-components'
+import Text from './Text'
 
 const VideoGrid = styled.div`
   width: 100%;
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  grid-gap: 36px;
+  grid-template-columns: 3fr 2fr;
+  grid-gap: 40px;
 `
 
 const VideoBlock = styled.div`
+  display: block;
   padding-top: 56.25%;
   background-color: #fff;
+`
+
+const VideoDescription = styled.div`
+  width: 100%;
 `
 
 const VideoGallery = () => {
   return (
     <VideoGrid>
       <VideoBlock />
-      <VideoBlock />
-      <VideoBlock />
-      <VideoBlock />
+      <VideoDescription>
+        <h2>Kelsi & Kristian</h2>
+        <h4>NY | Blue Hill at Stone Barns</h4>
+        <h3>
+          “I  believe — in a way that makes me feel both grounded and free — that I love you, and that I always will.”
+        </h3>
+      </VideoDescription>
     </VideoGrid>
   )
 }
