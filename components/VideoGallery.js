cdf46b52eb3928ec6b1705480692690b4349780f
Zach Cullen <zacullen@gmail.com>
2020-08-05 15:36:41 -0500

    restyle with specific spacing

--- components/VideoGallery.js
+++ components/VideoGallery.js
@@ -1,11 +1,10 @@
 import styled from 'styled-components'
-import Text from './Text'
 
 const VideoGrid = styled.div`
   width: 100%;
   display: grid;
-  grid-template-columns: 3fr 2fr;
-  grid-gap: 40px;
+  grid-template-columns: 5fr 3fr;
+  grid-gap: 50px;
 `
 
 const VideoBlock = styled.div`
@@ -16,6 +15,15 @@ const VideoBlock = styled.div`
 
 const VideoDescription = styled.div`
   width: 100%;
+  margin: 20px 0;
+
+  h2 {
+    margin-bottom: 4px;
+  }
+
+  h3 {
+    margin-bottom: 16px;
+  }
 `
 
 const VideoGallery = () => {
@@ -24,10 +32,10 @@ const VideoGallery = () => {
       <VideoBlock />
       <VideoDescription>
         <h2>Kelsi & Kristian</h2>
-        <h4>NY | Blue Hill at Stone Barns</h4>
-        <h3>
+        <h3>Blue Hill at Stone Barns</h3>
+        <h4>
           “I  believe — in a way that makes me feel both grounded and free — that I love you, and that I always will.”
-        </h3>
+        </h4>
       </VideoDescription>
     </VideoGrid>
   )
