17c62508b20d1f868a74402b169e495efa82d2da
Zach Cullen <zacullen@gmail.com>
2020-08-07 09:39:04 -0500

    remove unnecessary comments

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 import { useEffect, useRef } from 'react'
 
-// background-image sits beneath video overlay as fallback in unsupported mobile browsers
 const HeroDiv = styled.div`
   position: relative;
   z-index: -2;
@@ -12,7 +11,6 @@ const HeroDiv = styled.div`
   background-position: center;
   background-size: cover;
 `
-// places centered, responsive logo on top of overlay
 const HeroContent = styled.div`
   position: absolute;
   width: 50%;
@@ -28,7 +26,6 @@ const HeroContent = styled.div`
   background-size: contain;
 `
 
-// a semi-transparent layer over background video to increase contrast for content
 const HeroOverlay = styled.div`
   position: absolute;
   top: 0;
@@ -37,7 +34,6 @@ const HeroOverlay = styled.div`
   background-color: hsla(39, 30%, 55%, 0.34);
 `
 
-// necessary wrapper for creating responsive child iframe
 const BackgroundVideoWrapper = styled.div`
   position: fixed;
   top: 0;
@@ -49,8 +45,6 @@ const BackgroundVideoWrapper = styled.div`
   overflow: hidden;
 `
 
-// min-height calculation creates necessary slightly oversize
-// height and min-width calculations assume 16:9 aspect ratio
 const BackgroundVideoIframe = styled.iframe`
   width: 100vw;
   height: 56.25vw;
@@ -63,23 +57,17 @@ const BackgroundVideoIframe = styled.iframe`
 `
 
 const Hero = ({ videoUrl, screenPercentage }) => {
-  // create reference to be used by BackgroundVideoIframe
   const iframeRef = useRef(null)
-  // sets css style top of ref as inline style in DOM as function of rate (decimal value 0 - 1) and the amount scrolled
   const applyParallax = (ref, parallaxRate) => {
     ref.current.style.top = `-${window.pageYOffset * parallaxRate}px`
   }
   useEffect(() => {
-    // listens for scroll event and applies parallax effect to iFrame component
     document.addEventListener('scroll', () => {
       applyParallax(iframeRef, 0.5)
     })
   })
 
   return (
-    // props videoUrl must be provided for video background to work.
-    // must use player.vimeo address with params: '?background=1&autoplay=1&loop=1&byline=0&title=0' not public url
-    // screenPercentage prop should be integer <= 100 and determines height of hero section as percentage of screen height
     <HeroDiv screenPercentage={screenPercentage}>
       <HeroContent />
       <HeroOverlay>
