252e4f8e4357f3cbd559265a3bf6ab6d60ac38f7
Zach Cullen <zacullen@gmail.com>
2020-08-09 18:47:34 -0500

    create empty sticky nav component

---
+++ components/Nav.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 50px;
+  background-color: #fff;
+  box-shadow: 0px 4px 16px 0px rgba(0,0,0,0.2);
+`
+
+export default Nav
