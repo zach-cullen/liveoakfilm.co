4f80941f61e8763d59adde9b3fb9b464ded06e8b
Zach Cullen <zacullen@gmail.com>
2020-08-09 19:18:51 -0500

    make nav sticky using effect hook listening for scroll position

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,27 @@
 import styled from 'styled-components'
+import { useEffect, useRef } from 'react'
 
-const Nav = styled.nav`
-  position: fixed;
+const NavGrid = styled.nav`
+  position: absolute;
   top: 0;
   width: 100%;
   height: 50px;
-  background-color: #fff;
+  background-color: ${props => props.theme.colors.stone};
   box-shadow: ${props => props.theme.effects.mdBoxShadow};
 `
+
+const Nav = () => {
+  const navRef = useRef(null)
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      if (window.pageYOffset > 600) {
+        navRef.current.style.position = 'fixed';
+      }
+    })
+  })
+  return (
+    <NavGrid ref={navRef} />
+  )
+}
 
 export default Nav
