4e4c6fce6af326212aa4ac25fec6987a4b30900a
Zach Cullen <zacullen@gmail.com>
2020-08-17 17:20:51 -0500

    remove mobile styles and add section container to nav contents

--- components/Nav.js
+++ components/Nav.js
@@ -1,5 +1,6 @@
 import styled, { keyframes } from 'styled-components'
 import { useEffect, useState } from 'react'
+import SectionContainer from './SectionContainer'
 
 const fadeIn = keyframes`
   0% { opacity: 0; }
@@ -15,15 +16,6 @@ const NavGrid = styled.nav`
   box-shadow: ${props => props.fixedNav ? props.theme.effects.mdBoxShadow : ''};
   animation: ${props => props.fixedNav ? fadeIn : ''} 0.4s ease-in-out;
   color: ${props => props.fixedNav ? props.theme.colors.charcoal : '#fff'};
-
-  @media (max-width: 720px) {
-    position: fixed;
-    background-color: #fff;
-    height: 72px;
-    top: ${props => props.windowHeight - 72}px};
-    color: ${props => props.theme.colors.charcoal};
-    animation: '';
-  }
 `
 
 const NavContents = () => (
@@ -34,19 +26,17 @@ const NavContents = () => (
 
 const Nav = () => {
   const [scrollValue, setScrollValue] = useState(0)
-  const [windowHeight, setWindowHeight] = useState(0)
   const fixedNav = scrollValue => scrollValue > 600
   useEffect(() => {
     document.addEventListener('scroll', () => {
       setScrollValue(window.pageYOffset)
     })
   }, [])
-  useEffect(() => {
-    setWindowHeight(window.innerHeight)
-  })
   return (
-    <NavGrid fixedNav={fixedNav(scrollValue)} windowHeight={windowHeight}>
-      <NavContents />
+    <NavGrid fixedNav={fixedNav(scrollValue)}>
+      <SectionContainer>
+        <NavContents />
+      </SectionContainer>
     </NavGrid>
   )
 }
