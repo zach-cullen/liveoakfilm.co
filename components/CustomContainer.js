ce2a949d95ea3884500e9e6cea868a195ca11999
Zach Cullen <zacullen@gmail.com>
2020-08-18 15:03:43 -0500

    add padding options to custom container programmatically with props pX and pY

--- components/CustomContainer.js
+++ components/CustomContainer.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 const CustomContainer = styled.div`
   max-width: ${props => props.maxW};
   margin: 0 auto;
+  padding: ${props => props.pY ? props.pY : 0} ${props => props.pX ? props.pX : 0};
 `
 
 export default CustomContainer
