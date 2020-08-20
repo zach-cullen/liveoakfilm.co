eda39d30104fd63e92290ed43afae50e53fc281c
Zach Cullen <zacullen@gmail.com>
2020-08-20 11:25:00 -0500

    render iframe component and autoplay video from url prop

--- components/VideoFullScreen.js
+++ components/VideoFullScreen.js
@@ -1,16 +1,6 @@
 import styled from 'styled-components'
 import { CSSTransition } from 'react-transition-group'
 
-const FullScreenOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: #fff;
-  z-index: 150;
-`
-
 const ExpandTransition = styled.div`
   .expand--enter {
     opacity: 0;
@@ -18,13 +8,13 @@ const ExpandTransition = styled.div`
   }
 
   .expand--enter-active {
-    opacity: 0.9;
+    opacity: 1;
     height: 100%;
-    transition: height 0.4s ease-out, opacity 0.4s ease-out;
+    transition: height 0.5s ease-out, opacity 0.5s ease-out;
   }
 
   .expand--enter-done {
-    opacity: 0.9;
+    opacity: 1;
     height: 100%;
   }
 
@@ -42,17 +32,49 @@ const ExpandTransition = styled.div`
   }
 `
 
+const FullScreenOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 150;
+  background-color: hsla(0, 0%, 100%, 0.9);
+`
+
+const VideoIFrame = styled.iframe`
+  position: fixed;
+  z-index: 200;
+  top: 50%;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 90vw;
+  height: 50.85vw;
+  box-sizing: border-box;
+  border: solid 10px #fff;
+  background-color: #fff;
+`
+
 const VideoFullScreen = ({ show, closeVideoFullScreen, videoUrl }) => (
-  <ExpandTransition>
-    <CSSTransition
-      in={show}
-      timeout={500}
-      classNames='expand-'
-      unmountOnExit
-    >
-      <FullScreenOverlay onClick={closeVideoFullScreen} />
-    </CSSTransition>
-  </ExpandTransition>
+  <>
+    <ExpandTransition>
+      <CSSTransition
+        in={show}
+        timeout={500}
+        classNames='expand-'
+        unmountOnExit
+      >
+        <FullScreenOverlay onClick={closeVideoFullScreen}>
+          <VideoIFrame
+            src={videoUrl + '?autoplay=1'}
+            allow='autoplay'
+            frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen
+          />
+        </FullScreenOverlay>
+      </CSSTransition>
+    </ExpandTransition>
+  </>
 )
 
 export default VideoFullScreen
