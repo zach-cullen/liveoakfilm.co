a2b63471c78346dda1e81bb63c66bff073222fa1
Zach Cullen <zacullen@gmail.com>
2020-08-20 09:43:45 -0500

    add state hooks managing full screen video display and pass toggle function as props to child components

--- components/VideoGallery.js
+++ components/VideoGallery.js
@@ -1,10 +1,21 @@
+import { useState } from 'react'
 import FeaturedVideo from './FeaturedVideo'
+import VideoFullScreen from './VideoFullScreen'
 
 const VideoGallery = ({ filmCollections }) => {
+  const [showVideoFullScreen, setShowVideoFullScreen] = useState(false)
+  const toggleShowVideoFullScreen = () => setShowVideoFullScreen(prevState => !prevState)
+
   return (
     <>
+      <VideoFullScreen show={showVideoFullScreen} closeClickHandler={toggleShowVideoFullScreen} />
       {filmCollections.map((filmCollection, i) => (
-        <FeaturedVideo key={filmCollection.id} filmCollection={filmCollection} flipDirection={(i + 1) % 2 === 0} />
+        <FeaturedVideo
+          key={filmCollection.id}
+          filmCollection={filmCollection}
+          flipDirection={(i + 1) % 2 === 0}
+          playClickHandler={toggleShowVideoFullScreen}
+        />
       ))}
     </>
   )
