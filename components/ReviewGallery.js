f28c990dc1020dc74ff0fbaf362c863789ed1da0
Zach Cullen <zacullen@gmail.com>
2020-08-25 14:59:22 -0500

    create ReviewGallery component to render reviews with text and images

---
+++ components/ReviewGallery.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+const ReviewGalleryContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 40px;
+
+  @media (max-width: 720px) {
+    grid-template-columns: 1fr;
+    grid-row-gap: 20px;
+  }
+`
+
+const ReviewImg = styled.div`
+  width: 100%;
+  height: 80px;
+  background-image: url('http://localhost:1337${props => props.reviewImageUrl}');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+`
+
+const ReviewText = styled.div`
+  width: 100%;
+  height: 200px;
+  margin-top: 10px;
+`
+
+const Review = styled.div`
+  text-align: center;
+
+  p {
+    padding-top: 10px;
+  }
+`
+
+const ReviewGallery = ({ reviews }) => (
+  <ReviewGalleryContainer>
+    {reviews.map(review => (
+      <Review key={review.id}>
+        <div>
+          <ReviewImg reviewImageUrl={review.image[0].url} />
+          <p>{review.name}</p>
+        </div>
+        <ReviewText>
+          <h4>"{review.review}"</h4>
+        </ReviewText>
+      </Review>
+    ))}
+  </ReviewGalleryContainer>
+)
+
+export default ReviewGallery
