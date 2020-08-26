a2e6fa09787e6cd7ac689315ce8d1001884d43af
Zach Cullen <zacullen@gmail.com>
2020-08-26 15:44:12 -0500

    add section container and display white logo

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,27 @@
 import styled from 'styled-components'
+import SectionContainer from './SectionContainer'
+import NavLogoSVG from './NavLogoSVG'
 
-const Footer = styled.footer`
+const StyledFooter = styled.footer`
   width: 100%;
   box-sizing: border-box;
   padding: 50px 0;
   background-color: ${props => props.theme.colors.charcoal};
-  color: white;
 `
+
+const LogoWrapper = styled.div`
+  width: 60px;
+  margin: 0 auto;
+`
+
+const Footer = () => (
+  <StyledFooter>
+    <SectionContainer pY='50px'>\
+      <LogoWrapper>
+        <NavLogoSVG fill='#fff' />
+      </LogoWrapper>
+    </SectionContainer>
+  </StyledFooter>
+)
 
 export default Footer
