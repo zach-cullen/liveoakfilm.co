eafddc548223ab616d2947371975e61f6ff476f1
Zach Cullen <zacullen@gmail.com>
2020-08-26 17:21:53 -0500

    move head and main to next _document loader for dry pages

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,22 @@
-import Document from 'next/document'
+import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
+  render () {
+    return (
+      <html>
+        <Head>
+          <title>Live Oak Films</title>
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+
   static async getInitialProps (ctx) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+// import Head from 'next/head'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
@@ -13,49 +13,43 @@ import Text from '../components/Text'
 export default function Home ({ pageContent }) {
   return (
     <>
-      <Head>
-        <title>Live Oak Films</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <main>
-        <Nav />
-        <Hero
-          screenPercentage={66}
-          videoUrl='https://player.vimeo.com/video/227477654?background=1&autoplay=1&loop=1&byline=0&title=0'
-        />
-        <PageSection
-          bgColor={props => props.theme.colors.stone}
-        >
-          <SectionContainer textAlign='center'>
-            <CustomContainer maxW='540px'>
-              <h1>
-                Love should be remembered.
-              </h1>
-              <Text pY='30px'>
-                These are the memories that you want to last forever. This is the feeling that you don't want to forget. We are here to help you capture the best moments of your life because a love that is remembered is a <strong>love that leaves a legacy.</strong>
-              </Text>
-            </CustomContainer>
-            <Button themeColor='amber'>
-              Let us tell your story
-            </Button>
-          </SectionContainer>
-        </PageSection>
-        <PageSection
-          bgColor={props => props.theme.colors.khaki}
-        >
-          <CustomContainer maxW='1200px' pX='20px'>
-            <VideoGallery filmCollections={pageContent.film_collections} />
+      <Nav />
+      <Hero
+        screenPercentage={66}
+        videoUrl='https://player.vimeo.com/video/227477654?background=1&autoplay=1&loop=1&byline=0&title=0'
+      />
+      <PageSection
+        bgColor={props => props.theme.colors.stone}
+      >
+        <SectionContainer textAlign='center'>
+          <CustomContainer maxW='540px'>
+            <h1>
+              Love should be remembered.
+            </h1>
+            <Text pY='30px'>
+              These are the memories that you want to last forever. This is the feeling that you don't want to forget. We are here to help you capture the best moments of your life because a love that is remembered is a <strong>love that leaves a legacy.</strong>
+            </Text>
           </CustomContainer>
-          <Button themeColor='charcoal'>
-              Check Availability
+          <Button themeColor='amber'>
+            Let us tell your story
           </Button>
-        </PageSection>
-        <PageSection>
-          <SectionContainer pY='80px'>
-            <ReviewGallery reviews={pageContent.reviews} />
-          </SectionContainer>
-        </PageSection>
-      </main>
+        </SectionContainer>
+      </PageSection>
+      <PageSection
+        bgColor={props => props.theme.colors.khaki}
+      >
+        <CustomContainer maxW='1200px' pX='20px'>
+          <VideoGallery filmCollections={pageContent.film_collections} />
+        </CustomContainer>
+        <Button themeColor='charcoal'>
+            Check Availability
+        </Button>
+      </PageSection>
+      <PageSection>
+        <SectionContainer pY='80px'>
+          <ReviewGallery reviews={pageContent.reviews} />
+        </SectionContainer>
+      </PageSection>
       <Footer />
     </>
   )
