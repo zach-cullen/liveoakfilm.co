c30b32ce68d6ca240922cca5d1ec796e9d4a88f7
Zach Cullen <zacullen@gmail.com>
2020-08-26 17:27:25 -0500

    render basic page for about with nav, footer, hero, and empty section

--- components/PageSection.js
+++ components/PageSection.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const PageSection = styled.div`
   width: 100%;
   padding: 60px 0;
-  background-color: ${props => props.bgColor};
+  background-color: ${props => props.bgColor ? props.bgColor : props.theme.colors.stone};
 `
 
 export default PageSection
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,18 @@
-export default function Home ({ pageContent }) {
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
+import Hero from '../components/Hero'
+import PageSection from '../components/PageSection'
+
+export default function About ({ pageContent }) {
   return (
-    <h1>About</h1>
+    <>
+      <Nav />
+      <Hero
+        screenPercentage={50}
+        videoUrl='https://player.vimeo.com/video/227477654?background=1&autoplay=1&loop=1&byline=0&title=0'
+      />
+      <PageSection />
+      <Footer />
+    </>
   )
 }
