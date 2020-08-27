7f4478d66917fbb5125e503c8bcc768eec2e875a
Zach Cullen <zacullen@gmail.com>
2020-08-27 08:38:17 -0500

    enable custom container to set text alignment with prop

--- components/CustomContainer.js
+++ components/CustomContainer.js
@@ -4,6 +4,7 @@ const CustomContainer = styled.div`
   max-width: ${props => props.maxW};
   margin: 0 auto;
   padding: ${props => props.pY ? props.pY : 0} ${props => props.pX ? props.pX : 0};
+  text-align: ${props => props.textAlign};
 `
 
 export default CustomContainer
