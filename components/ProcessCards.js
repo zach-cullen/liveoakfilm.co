d1ab086e77de52964cda7b46eb4a652dd9e66791
Zach Cullen <zacullen@gmail.com>
2020-08-27 09:06:04 -0500

    create ProcessCards component rendering empty full width container

---
+++ components/ProcessCards.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const CardGrid = styled.div`
+  width: 100%;
+  height: 200px;
+  margin: 30px 0;
+  background-color: #fff;
+`
+
+const ProcessCards = () => (
+  <CardGrid />
+)
+
+export default ProcessCards
