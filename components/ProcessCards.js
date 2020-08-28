0a87669e103d993630cb079b496da0b3900dfdcf
Zach Cullen <zacullen@gmail.com>
2020-08-27 19:19:23 -0500

    render 3 cards to fill columns in card grid

--- components/ProcessCards.js
+++ components/ProcessCards.js
@@ -2,13 +2,24 @@ import styled from 'styled-components'
 
 const CardGrid = styled.div`
   width: 100%;
-  height: 200px;
-  margin: 30px 0;
+  margin: 40px 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 30px;
+`
+
+const ProcessCard = styled.div`
+  min-height: 300px;
   background-color: #fff;
+  box-shadow: ${props => props.theme.effects.mdBoxShadow};
 `
 
 const ProcessCards = () => (
-  <CardGrid />
+  <CardGrid>
+    <ProcessCard />
+    <ProcessCard />
+    <ProcessCard />
+  </CardGrid>
 )
 
 export default ProcessCards
