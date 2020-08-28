7e56eff3b8536dafd11736974e48ebab779327c3
Zach Cullen <zacullen@gmail.com>
2020-08-28 17:28:08 -0500

    refactor styles for declarative recomposition of mobile layout

--- components/TeamMembers.js
+++ components/TeamMembers.js
@@ -10,30 +10,45 @@ const TeamMember = styled.div`
   margin-bottom: 80px;
   display: grid;
   grid-template-columns: 1fr 3fr;
+  grid-template-rows: 60px 1fr;
   grid-column-gap: 50px;
-`
+  grid-template-areas:
+    "head name"
+    "head bio";
 
-const Profile = styled.div`
-  text-align: left;
+  @media(max-width: 720px) {
+    grid-template-columns: 1fr 3fr;
+    grid-template-rows: 100px 1fr;
+    grid-column-gap: 20px;
+    grid-row-gap: 20px;
+    grid-template-areas:
+      "head name"
+      "bio bio";
+  }
 `
 
 const Headshot = styled.div`
+  grid-area: head;
+  min-height: 100px;
+  min-width: 100px;
   background-image: url('http://localhost:1337${props => props.headshotUrl}');
   background-repeat: no-repeat;
   background-position: top;
   background-size: contain;
+
+  @media(max-width: 720px) {
+    background-position: left;
+  }
 `
 
-const Name = styled.h3`
-  font-size: 20px;
-`
-
-const Title = styled.h4`
-  margin-bottom: 20px;
+const NameTitle = styled.div`
+  margin: auto 0;
+  text-align: left;
 `
 
 const Bio = styled.div`
-
+  grid-area: bio;
+  margin-top: 20px;
 `
 
 const TeamMembers = ({ teamMembers }) => (
@@ -41,19 +56,15 @@ const TeamMembers = ({ teamMembers }) => (
     {teamMembers.map(teamMember => (
       <TeamMember key={teamMember.name}>
         <Headshot headshotUrl={teamMember.headshot.url} />
-        <Profile>
-          <Name>
-            {teamMember.name}
-          </Name>
-          <Title>
-            {teamMember.title}
-          </Title>
-          <Bio>
-            <Text textAlign='left'>
-              {teamMember.bio}
-            </Text>
-          </Bio>
-        </Profile>
+        <NameTitle>
+          <h3>{teamMember.name}</h3>
+          <h4>{teamMember.title}</h4>
+        </NameTitle>
+        <Bio>
+          <Text textAlign='left'>
+            {teamMember.bio}
+          </Text>
+        </Bio>
       </TeamMember>
     ))}
   </TeamContainer>
