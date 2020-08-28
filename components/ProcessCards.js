a8933b5bc9bcc1aca289dfd498c3a05b96d44247
Zach Cullen <zacullen@gmail.com>
2020-08-27 19:59:55 -0500

    add heading and text to cards and make layout responsive

--- components/ProcessCards.js
+++ components/ProcessCards.js
@@ -1,24 +1,54 @@
 import styled from 'styled-components'
+import Text from '../components/Text'
 
 const CardGrid = styled.div`
   width: 100%;
   margin: 40px 0;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  grid-column-gap: 30px;
+  grid-gap: 30px;
+
+  @media(max-width: 800px) {
+    grid-template-columns: 1fr;
+  }
 `
 
 const ProcessCard = styled.div`
-  min-height: 300px;
+  box-sizing: border-box;
+  padding: 60px 30px;
   background-color: #fff;
-  box-shadow: ${props => props.theme.effects.mdBoxShadow};
+`
+
+const CardTitle = styled.h2`
+  margin-bottom: 30px;
+`
+
+const CardText = styled.h4`
+  display: block;
+  font-size: 20px;
+  text-align: left;
 `
 
 const ProcessCards = () => (
   <CardGrid>
-    <ProcessCard />
-    <ProcessCard />
-    <ProcessCard />
+    <ProcessCard>
+      <CardTitle>Tell us your story.</CardTitle>
+      <CardText>
+        We want to get to know you, the people you love and the moments you can't wait for. Then, we'll work together to make sure the day flows effortlessly.
+      </CardText>
+    </ProcessCard>
+    <ProcessCard>
+      <CardTitle>Have the best day.</CardTitle>
+      <CardText>
+        Our first priority is that you get to enjoy and take in your special day. We promise that if you're having fun then we are absolutely making a great video.
+      </CardText>
+    </ProcessCard>
+    <ProcessCard>
+      <CardTitle>Laugh, cry, share.</CardTitle>
+      <CardText>
+        Our edits are simple, we aim to let your moments speak for themselves. We capture the full range of emotion from silly to sappy and everything inbetween.
+      </CardText>
+    </ProcessCard>
   </CardGrid>
 )
 
