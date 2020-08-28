7c61b314659d5dba53b00f554933ea11f879de8c
Zach Cullen <zacullen@gmail.com>
2020-08-27 20:08:38 -0500

    create empty team members component use by about page

---
+++ components/TeamMembers.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const TeamContainer = styled.div`
+  height: 200px;
+  margin: 50px 0;
+  background-color: #fff;
+`
+
+const TeamMembers = () => (
+  <TeamContainer />
+)
+
+export default TeamMembers
