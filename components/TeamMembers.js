b23fcd6e5f5faf67c17a58e86ac3ec9a6fa6c137
Zach Cullen <zacullen@gmail.com>
2020-08-28 14:13:30 -0500

    map teamMember data to child components displaying name

--- components/TeamMembers.js
+++ components/TeamMembers.js
@@ -1,13 +1,23 @@
 import styled from 'styled-components'
 
 const TeamContainer = styled.div`
-  height: 200px;
   margin: 50px 0;
+`
+
+const TeamMember = styled.div`
+  height: 100px;
+  margin: 20px 0;
   background-color: #fff;
 `
 
-const TeamMembers = () => (
-  <TeamContainer />
+const TeamMembers = ({ teamMembers }) => (
+  <TeamContainer>
+    {teamMembers.map(teamMember => (
+      <TeamMember key={teamMember.name}>
+        {teamMember.name}
+      </TeamMember>
+    ))}
+  </TeamContainer>
 )
 
 export default TeamMembers
