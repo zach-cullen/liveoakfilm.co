c6a9a0128555324d743f59c5b394c475549239e3
Zach Cullen <zacullen@gmail.com>
2020-08-29 11:58:17 -0500

    allow page section to set custom vertical padding with prop or use default

--- components/PageSection.js
+++ components/PageSection.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 const PageSection = styled.div`
   width: 100%;
-  padding: 60px 0;
+  padding: ${props => props.pY ? props.pY : '60px'} 0;
   background-color: ${props => props.bgColor ? props.bgColor : props.theme.colors.stone};
 `
 
