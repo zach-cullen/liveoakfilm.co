714fd8287346eb7f8e25ad4b6adf1e3e6d899ade
Zach Cullen <zacullen@gmail.com>
2020-08-30 16:31:25 -0500

    remove redundant sizes object from theme provider

--- theme/theme.js
+++ theme/theme.js
@@ -5,17 +5,6 @@ export default {
     stone: '#f8f5f1',
     khaki: '#E4DBCC'
   },
-  sizes: {
-    text: {
-      p: 20,
-      h6: 12,
-      h5: 16,
-      h4: 20,
-      h3: 24,
-      h2: 32,
-      h1: 48
-    }
-  },
   effects: {
     mdBoxShadow: '0px 4px 16px 0px rgba(0,0,0,0.2)'
   }
