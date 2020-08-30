f05f809da51b6d96399cca1d0993aa044285c93e
Zach Cullen <zacullen@gmail.com>
2020-08-30 07:33:14 -0500

    add Hero section to inquire page

--- pages/inquire.js
+++ pages/inquire.js
@@ -1,16 +1,21 @@
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
+import Hero from '../components/Hero'
 import PageSection from '../components/PageSection'
 import CustomContainer from '../components/CustomContainer'
 
-export default function Portfolio ({ pageContent }) {
+export default function Inquire ({ pageContent }) {
   return (
     <>
-      <Nav noHero />
-      <PageSection pY='40px' />
+      <Nav />
+      <Hero
+        overlayImgUrl='/images/contact-header.png'
+        screenPercentage={50}
+        videoUrl='https://player.vimeo.com/video/227477654?background=1&autoplay=1&loop=1&byline=0&title=0'
+      />
       <PageSection>
-        <CustomContainer maxW='1200px' pX='30px' textAlign='center'>
-          <h1>Let's Chat</h1>
+        <CustomContainer maxW='1200px' pX='30px' pY='30px' textAlign='center'>
+          <h3>Wedding Inquiries:</h3>
         </CustomContainer>
       </PageSection>
       <Footer />
